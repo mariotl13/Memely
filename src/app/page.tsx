5dfbe0d4125b99edb5c0ac3fb34375a594c508fe
DudeArtero <developersaj@gmail.com>
2024-05-26 12:11:38 +0200

    test openai

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,34 @@
 "use client"
+import OpenAI from "openai";
 import { useEffect, useState } from "react";
+
+const openai = new OpenAI({
+  apiKey: process.env['NEXT_PUBLIC_OPENAI_API_KEY'], // This is the default and can be omitted,
+  dangerouslyAllowBrowser: true
+});
 
 export default function Home() {
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<any>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://api.openai.com/v1/chat/completions", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-          },
+        const chatCompletion = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo-16k",
+          messages: [
+            {
+              "role": "system",
+              "content": "You are a helpful assistant."
+            },
+            {
+              "role": "user",
+              "content": "Hello!"
+            }
+          ]
         });
 
-        const result = await response.json();
-        setData(result);
+        setData(chatCompletion);
 
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -31,7 +42,9 @@ export default function Home() {
 
   return (
     <main>
-      <div>{process.env.OPENAI_API_KEY}</div>
+      <div>
+        {data.result?.choices?.[0].content}
+      </div>
     </main>
   );
 }
