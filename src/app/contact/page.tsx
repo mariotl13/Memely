9d954d69da5e88088f71283bc0313d5555681063
DudeArtero <developersaj@gmail.com>
2024-05-26 11:20:50 +0200

    test openai

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+import "./styles.scss";
+
+export default function Contact() {
+    return <div className="card">
+        <h5 className="card__title">Título de la card</h5>
+        <p className="card__content">Contenido de la card.</p>
+    </div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+          },
+        });
+
+        const result = await response.json();
+        setData(result);
+
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <main>
-      
+      <div>{process.env.OPENAI_API_KEY}</div>
     </main>
   );
 }
