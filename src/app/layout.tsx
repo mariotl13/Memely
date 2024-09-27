b7ac441b32ae5b8cfcc9cb95fb371fac04b73c30
Mario Tapia <mtapialopez@deloitte.es>
2024-09-27 14:37:16 +0200

    test fix ios

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,22 +3,23 @@ import { Outfit } from "next/font/google";
 import "./globals.scss";
 import Landing from "./landing/landing";
 
+import "@formatjs/intl-datetimeformat/polyfill";
+import "@formatjs/intl-datetimeformat/locale-data/en"; // locale-data for en
+import "@formatjs/intl-datetimeformat/locale-data/es"; // locale-data for es
+import "@formatjs/intl-datetimeformat/add-all-tz"; // Add ALL tz data
+
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Meme.ly",
-    description: "There is a meme for everything, and if not, you will create it.",
+	title: "Meme.ly",
+	description:
+		"There is a meme for everything, and if not, you will create it.",
 };
 
 export default function RootLayout({
-    children,
+	children,
 }: Readonly<{
-    children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-
-    return (
-        <Landing inter={outfit}>
-            {children}
-        </Landing>
-    );
+	return <Landing inter={outfit}>{children}</Landing>;
 }
