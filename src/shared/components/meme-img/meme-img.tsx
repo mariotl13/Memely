3c073fb47fce1129972f8313f495b19380b72a0e
Mario Tapia <mtapialopez@deloitte.es>
2024-10-24 09:52:43 +0200

    Add zoom background

--- src/shared/components/meme-img/meme-img.tsx
+++ src/shared/components/meme-img/meme-img.tsx
@@ -2,10 +2,11 @@
 
 import Zoom from "react-medium-image-zoom";
 import "react-medium-image-zoom/dist/styles.css";
+import "./meme-img.scss";
 
 const MemeImg = ({ src, alt }: { src: string; alt: string }) => {
 	return (
-		<Zoom>
+		<Zoom classDialog="zoom-dialog">
 			<img src={src} alt={alt} />
 		</Zoom>
 	);
